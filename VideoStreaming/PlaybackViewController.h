3fac1c5393fe98d8c5092f5a5cf9565f294edf04
Moosa Baloch <muhammad.moosa@koderlabs.com>
2020-04-13 01:48:53 +0500

    Added Ignore

---
+++ VideoStreaming/PlaybackViewController.h
@@ -0,0 +1,17 @@
+//
+//  PlaybackViewController.h
+//  VideoStreaming
+//
+//  Created by Moosa Baloch on 10/04/2020.
+//  Copyright © 2020 KoderLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlaybackViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ VideoStreaming/PlaybackViewController.m
@@ -0,0 +1,32 @@
+//
+//  PlaybackViewController.m
+//  VideoStreaming
+//
+//  Created by Moosa Baloch on 10/04/2020.
+//  Copyright © 2020 KoderLabs. All rights reserved.
+//
+
+#import "PlaybackViewController.h"
+
+@interface PlaybackViewController ()
+
+@end
+
+@implementation PlaybackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
